9f3262664827834bc80ae7b75ea9c9c3212ddcce
pedrohp28 <pedrohp36@gmail.com>
2024-07-16 04:29:42 -0300

    Transformando função em API

--- index.mjs
+++ index.mjs
@@ -5,6 +5,9 @@ export const handler = async (event) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
+        headers: {
+            "content-type": "text/html"
+        }
       };
   };
